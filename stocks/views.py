dffea3377e4b1b011d68ff48be051c8a9c950c94
pmontu <pmontu@gmail.com>
2019-11-16 19:56:35 +0530

    celery

--- stocks/views.py
+++ stocks/views.py
@@ -1,8 +1,13 @@
 from .serializers import ProductSerializer
 from rest_framework.viewsets import ModelViewSet
 from .models import Product
+from StockManager.celery import debug_task
 
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        debug_task()
+        return super().list(request, *args, **kwargs)
