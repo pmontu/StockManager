61783ec08dac70f98dbc3faf8528765748ddb82e
pmontu <pmontu@gmail.com>
2019-11-16 12:39:06 +0530

    http://localhost:8000/static/index.html created react app & built into django staticfiles

--- StockManager/settings.py
+++ StockManager/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'StockManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stock_manager',
+        'USER': 'manoj',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
