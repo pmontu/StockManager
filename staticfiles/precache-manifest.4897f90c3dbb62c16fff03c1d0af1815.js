431abc3aa94583cb63c0e8f4ee3313dc12639816
pmontu <pmontu@gmail.com>
2019-11-16 13:15:34 +0530

    staticfiles added

---
+++ staticfiles/precache-manifest.4897f90c3dbb62c16fff03c1d0af1815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da55ec3ac8012fa2448c563453db9331",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e34166456a7a303621f",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6c89c4a3253c592a2ae2",
+    "url": "./static/js/2.a723b85c.chunk.js"
+  },
+  {
+    "revision": "5e34166456a7a303621f",
+    "url": "./static/js/main.ed59cd5f.chunk.js"
+  },
+  {
+    "revision": "8020b650e5f5db8a485c",
+    "url": "./static/js/runtime-main.62e41b83.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
