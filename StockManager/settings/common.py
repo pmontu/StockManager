f7968165a7c1f13f2609df68479be2bb1249298f
pmontu <pmontu@gmail.com>
2019-11-23 18:11:13 +0530

    you should create your own dev settings file & use --settings=StockManager.settings.dev

--- StockManager/settings/common.py
+++ StockManager/settings/common.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET"]
+SECRET_KEY = os.environ.get("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
