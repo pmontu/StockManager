33464e0fdb39de532c1d304ee8cfd84cccfab26a
pmontu <pmontu@gmail.com>
2019-11-23 18:18:27 +0530

    settings need to be modified for BASE_DIR & PROJECT_ROOT to work correctly after last commit

--- StockManager/settings/common.py
+++ StockManager/settings/common.py
@@ -14,8 +14,9 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
