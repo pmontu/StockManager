b2cb97333f194e791fe79f8a00e15ab454356509
pmontu <pmontu@gmail.com>
2019-12-20 22:26:06 +0530

    fetch product with error

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { baseURL } from "../config";
 
 function Product() {
-  return <div>hi</div>;
+  let { id } = useParams();
+  let [isLoading, setIsLoading] = useState(false);
+  let [error, setError] = useState(undefined);
+  let [product, setProduct] = useState({});
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`${baseURL}/stocks/products/${id}`)
+      .then(res => {
+        if (!res.ok) throw new Error(`${res.status}:${res.statusText}`);
+        return res.json();
+      })
+      .then(json => {
+        setProduct(json);
+        setIsLoading(false);
+      })
+      .catch(e => {
+        setError(e);
+      });
+  }, [id]);
+
+  if (error) return `${error}`;
+  if (isLoading) return "loading...";
+  return <div>{id}</div>;
 }
 
 export default Product;
--- src/Products/ProductList.js
+++ src/Products/ProductList.js
@@ -9,19 +9,28 @@ function ProductList() {
   let [isLoadingTable, setIsLoadingTable] = useState(false);
   let [maxPages, setMaxPages] = useState(-1);
   let [page, setPage] = useState(1);
+  let [error, setError] = useState(undefined);
   let history = useHistory();
 
   useEffect(() => {
     setIsLoadingTable(true);
     fetch(`${baseURL}/stocks/products/?page=${page}`)
-      .then(res => res.json())
+      .then(res => {
+        if (!res.ok) throw new Error(`${res.status}:${res.statusText}`);
+        return res.json();
+      })
       .then(json => {
         setProducts(json.results);
         setMaxPages(Math.ceil(json.count / PAGE_SIZE));
         setIsLoadingTable(false);
+      })
+      .catch(e => {
+        setError(e);
       });
   }, [page]);
 
+  if (error) return `${error}`;
+  if (isLoadingTable) return "loading...";
   if (maxPages === -1) return "no data yet";
 
   return (
